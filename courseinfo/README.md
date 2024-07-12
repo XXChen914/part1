26c6e2f721f37557c5c5e14ca42790c9628a98b9
Xiaoxu chen <xiaoxuchen@Xiaoxus-MacBook-Air.local>
2024-07-12 15:21:41 -0700

    Summiting 1.1-1.5

---
+++ courseinfo/README.md
@@ -0,0 +1,3 @@
+# Course Info
+## Part 1
+Solutions of Exercise 1.1 - 1.5
---
+++ courseinfo/src/App.jsx
@@ -0,0 +1,52 @@
+const Header = ({ title }) => <h1>{title}</h1>;
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part part={part} />
+      ))}
+    </div>
+  );
+};
+
+const Total = ({ parts }) => {
+  const result = parts.reduce((sum, part) => sum + part.exercises, 0);
+  return <p>Number of exercises {result}</p>;
+};
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header title={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default App;
